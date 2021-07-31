2afa6360b645e5a34ea5cc9c9f2894dae0f56b0b
nickrepaci <nickrepaci@gmail.com>
2021-07-31 16:40:05 -0400

    Added line animations and button click animation

--- ChartTracker/ViewController.swift
+++ ChartTracker/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var getChart: UIButton!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var tickerTextField: UITextField!
     @IBOutlet weak var chartDetailsLabel: UILabel!
+    @IBOutlet weak var tickerLine: UIView!
+    @IBOutlet weak var dateLine: UIView!
     
     var selectedDate = ""
     
@@ -30,13 +32,14 @@ class ViewController: UIViewController {
         
         //keyboard dismissal funciton
         self.hideKeyboardWhenTappedAround()
+        animateLines()
         
     }
     
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func getChart(_ sender: Any) {
-        
+        animateGetChartButton()
         convertDateForApi()
         getDataFromApi()
         updateChartLabel()
@@ -46,29 +49,25 @@ class ViewController: UIViewController {
     func getDataFromApi() {
         
         let session = URLSession(configuration: URLSessionConfiguration.default)
-                
+        
         guard let url = URL(string: "http://api.tradingphysics.com/getchart?type=pi&date=\(selectedDate)&stock=\(tickerTextField.text ?? "")&days=1") else { return }
-                
-                var request = URLRequest(url: url)
-                request.httpMethod = "GET"
-                
-                session.dataTask(with: request) { (data, response, error) in
-                    
-                    if let error = error {
-                        print("Something went wrong: \(error)")
-                    }
-                    
-                    if let imageData = data {
-                        // The UI should be updated from the main thread
-                        DispatchQueue.main.async {
-                            self.imageView.image = UIImage(data: imageData)
-                        }
-                    }
-                }.resume()
         
-       // print(datePicker.date)
-       // convertDateForApi()
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
         
+        session.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                print("Something went wrong: \(error)")
+            }
+            
+            if let imageData = data {
+                // The UI should be updated from the main thread
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: imageData)
+                }
+            }
+        }.resume()
         
     }
     
@@ -90,7 +89,33 @@ class ViewController: UIViewController {
         sender.view?.transform = (sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale))!
         sender.scale = 1.0
     }
-
+    
+    func animateLines() {
+        UIView.animate(withDuration: 0.6,
+                       animations: {
+                        self.tickerLine.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+                        self.dateLine.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+                       },
+                       completion: { _ in
+                        UIView.animate(withDuration: 0.6) {
+                            self.tickerLine.transform = CGAffineTransform.identity
+                            self.dateLine.transform = CGAffineTransform.identity
+                        }
+                       })
+    }
+    
+    func animateGetChartButton() {
+        UIView.animate(withDuration: 0.09,
+                       animations: {
+                        self.getChart.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                       },
+                       completion: { _ in
+                        UIView.animate(withDuration: 0.09) {
+                            self.getChart.transform = CGAffineTransform.identity
+                        }
+                       })
+    }
+    
 }
 
 
@@ -99,9 +124,9 @@ extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
-
+        
     }
-
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
