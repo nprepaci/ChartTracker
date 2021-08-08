f16f463e32e30d262086ebc320b64f5b138906c7
nickrepaci <nickrepaci@gmail.com>
2021-08-08 16:38:00 -0400

    Added unit test for API

--- ChartTrackerTests/ChartTrackerTests.swift
+++ ChartTrackerTests/ChartTrackerTests.swift
@@ -9,6 +9,32 @@ import XCTest
 @testable import ChartTracker
 
 class ChartTrackerTests: XCTestCase {
+    
+    func testAPI() {
+        
+        // Create an expectation for a background download task.
+        let expectation = XCTestExpectation(description: "Test if API is returning data")
+        
+        // Create a URL for a web page to be downloaded.
+        let url = URL(string: "http://api.tradingphysics.com/getchart?type=pi&date=20210804&stock=QQQ&days=1")!
+        
+        // Create a background task to download the web page.
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            // Make sure we downloaded some data.
+            XCTAssertNotNil(data, "No data was downloaded.")
+            
+            // Fulfill the expectation to indicate that the background task has finished successfully.
+            expectation.fulfill()
+            
+        }
+        
+        // Start the download task.
+        dataTask.resume()
+        
+        // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+        wait(for: [expectation], timeout: 10.0)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
